b8f5dc89c78409833a229ac2cfc67074db8ffb38
atongjonathan <atongjonathan@gmail.com>
2026-09-10 06:05:31 +0300

    feat: customize admin:

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import User, Listing
 
+
+admin.site.site_header = "Popote Listings Administration"
+admin.site.site_title = "Popote Listings Admin"
+admin.site.index_title = "Welcome to Popote Listings Administration"
 admin.site.register(User)
 admin.site.register(Listing)
